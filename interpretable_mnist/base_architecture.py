Fix improper distance calculation

interpretable_mnist/base_architecture.py
@@ -11,24 +11,21 @@ class SimpleConvNetRoot(torch.nn.Module):
         self.activation = activation
 
         # fmt: off
-        self.layers = torch.nn.ModuleList([                    # input: 1 x 28 x 28
-            torch.nn.Conv2d(1, 32, 3, padding="same"),         # 32 x 28 x 28
-            torch.nn.LazyBatchNorm2d(),
+        self.layers = torch.nn.ModuleList([                     # input: 1 x 28 x 28
+            torch.nn.Conv2d(1, 32, 2, stride=2, padding=0),     # 32 x 14 x 14
+            torch.nn.BatchNorm2d(32),
             self.activation,
-            torch.nn.MaxPool2d(2),                             # 32 x 14 x 14
-            torch.nn.Conv2d(32, 128, 3, padding="same"),       # 128 x 14 x 14
-            torch.nn.LazyBatchNorm2d(),
+            torch.nn.Conv2d(32, 128, 2, stride=2, padding=0),   # 128 x 7 x 7
+            torch.nn.BatchNorm2d(128),
             self.activation,
-            torch.nn.MaxPool2d(2),                             # 128 x 7 x 7
-            torch.nn.Conv2d(128, 256, 3, padding="same"),      # 256 x 7 x 7
-            torch.nn.LazyBatchNorm2d(),
+            torch.nn.Conv2d(128, 256, 2, stride=2, padding=0),  # 256 x 3 x 3
+            torch.nn.BatchNorm2d(256),
+            self.activation,                                    # 256 x 3 x 3
+            torch.nn.Conv2d(256, 128, 1, stride=1, padding=0),  # 128 x 3 x 3
+            torch.nn.BatchNorm2d(128),
             self.activation,
-            torch.nn.MaxPool2d(2),                             # 256 x 3 x 3
-            torch.nn.Conv2d(256, 128, 3, padding="same"),      # 128 x 3 x 3
-            torch.nn.LazyBatchNorm2d(),
-            self.activation,
-            torch.nn.Conv2d(128, 64, 3, padding="same"),       # 64 x 3 x 3
-            torch.nn.LazyBatchNorm2d(),
+            torch.nn.Conv2d(128, 64, 1, stride=1, padding=0),   # 64 x 3 x 3
+            torch.nn.BatchNorm2d(64),
             self.activation,
         ])
         # fmt: on

interpretable_mnist/proto_pnet.py
@@ -8,28 +8,6 @@ from interpretable_mnist.base_architecture import SimpleConvNetRoot
 from interpretable_mnist.core import ProjectedPrototype
 
 
-def _get_starting_output_layer_class_connections(n_classes: int, n_protos_per_class: int) -> torch.Tensor:
-    """
-    Get starting connections in fully connected layer. Make positive connections from the
-    prototype slots belonging to the class to the class output neuron and negative connections
-    from prototype slots of other classes.
-
-    :param n_classes: c - Amount of different classes
-    :param n_protos_per_class: p - Amount of prototypes per class
-    :return: [c * p, c] - Weights for properly connected linear layer
-    """
-
-    # slots not belonging to class are not set to -0.5 as per [2] because similarities to
-    # prototypes of other classes should not count as evidence against the class if prototypes are shared
-    starting_weights = torch.full((n_classes * n_protos_per_class, n_classes), fill_value=0.)
-
-    for i in range(n_protos_per_class * n_classes):
-        # connect first `n_slots_per_class` with the class "0", second `n_slots_per_class` with class "1", etc.
-        starting_weights[i, i // n_protos_per_class] = 1
-
-    return starting_weights
-
-
 def prototype_distances_to_similarities(distances: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
     """
     Convert prototype distances to similarities according to [1], i.e.
@@ -41,8 +19,7 @@ def prototype_distances_to_similarities(distances: torch.Tensor) -> tuple[torch.
 
     :param distances: [b: n_samples_batch c: n_classes p: n_proto_per_class, h: height, w: width] Prototype distances
         across (latent) image
-    :return: [b: n_samples_batch, c: n_classes p: n_proto_per_class] Similarity score of each prototype to the (latent)
-        image,
+    :return: [b: n_samples_batch, c: n_classes p: n_proto_per_class] Similarity score of each prototype to the latent,
         [b: n_samples_batch, c: n_classes, p: n_proto_per_class] Minimum distances for each prototype
     """
     min_distances = torch.min(torch.min(distances, dim=-1)[0], dim=-1)[0]  # [b, c, p]
@@ -119,55 +96,53 @@ class ProtoPNetMNIST(pl.LightningModule):
         """
         super().__init__()
 
+        self.minkowski_distance_order = train_info.minkowski_distance_order
         self.learning_rate = train_info.learning_rate
         self.n_classes = train_info.n_classes  # c - Amount of different classes to predict; 10 for MNIST
         self.n_protos_per_class = train_info.n_protos_per_class  # p - Amount of prototypes each class gets
 
         self.projection_epochs = train_info.projection_epochs
-        self.freeze_epoch = train_info.projection_epochs[-1]
+        self.last_epoch = train_info.projection_epochs[-1]
         self.cluster_loss_weight = train_info.cluster_loss_weight
         self.separation_loss_weight = train_info.separation_loss_weight
-        self.prototypes_shape = (self.n_classes, self.n_protos_per_class, prototype_depth, 1, 1)  # [c, p, d, 1, 1]
+
 
         # --- Setup convolution layers f: ---
         self.conv_root = SimpleConvNetRoot()  # outputs n_samples_batch x 64 x 3 x 3 -> dim [b, k, h, w]
 
         # --- Setup prototypes layer g: ---
-        self.prototypes = torch.nn.Parameter(torch.rand(self.prototypes_shape))  # [c, p, d, 1, 1]
+        prototypes_shape = (self.n_classes, self.n_protos_per_class, prototype_depth, 1, 1)  # [c, p, d, 1, 1]
+        self.prototypes = torch.nn.Parameter(torch.rand(prototypes_shape))  # [c, p, d, 1, 1]
         self.projected_prototypes = [[None] * self.n_protos_per_class for _ in range(self.n_classes)]  # [c, p]
 
-        # --- Setup fully connected output layer h: ---
-        self.output_layer = torch.nn.Linear(self.n_classes * self.n_protos_per_class, self.n_classes, bias=False)
-        self.output_layer.weight = torch.nn.Parameter(  # need to transpose to correctly fit into weights of layer
-            torch.t(_get_starting_output_layer_class_connections(self.n_classes, self.n_protos_per_class))
-        )
-        self.output_layer.requires_grad_(False)
+        # --- Setup weights that connect only class prototype to prediction for class: ---
+        self.output_weights = torch.nn.Parameter(torch.ones((self.n_classes, self.n_protos_per_class)))  # [c, p]
         # note: softmax already in loss function
 
     def _get_prototype_distances(self, z: torch.Tensor) -> torch.Tensor:
         """
-        Calculate distance between the latent image representation `z`
+        Calculate the minkowski distance of order f between the latent image representation `z`
         and the learned prototypes P, normalized to the prototype depth d.
         In brief:
 
-            return |z-P|**2 / d
+            return (sum_i |z_i-P_i|**f) ^ (1/f) / d
 
         :param z: [b: n_samples_batch, k: channels, h: height, w: width] - Latent image representation
         :return: [b: n_samples_batch, c: n_classes, p: n_protos_per_class, h: height, w: width] - (Normalized) distances
             to prototypes
         """
         # prototype shape: [c, p, d, 1, 1]
+        f = self.minkowski_distance_order
         z_minus_p = z[:, np.newaxis, np.newaxis, ...] - self.prototypes[np.newaxis, ...]  # [b, c, p, k, h, w]
-        return torch.sum(z_minus_p, dim=3) ** 2 / self.prototypes.shape[2]  # [b, c, p, h, w]
+        return (1 / self.prototypes.shape[2] * torch.sum(torch.abs(z_minus_p) ** f, dim=3)) ** (1/f)   # [b, c, p, h, w]
 
     def forward(self, x: torch.Tensor):
         z = self.conv_root(x)  # [b, k, h, w]
 
         prototype_distances = self._get_prototype_distances(z)  # [b, c, p, h, w]
         prototype_similarities, min_distances = prototype_distances_to_similarities(prototype_distances)  # [b, c, p]
-        prototype_similarities = torch.flatten(prototype_similarities, start_dim=1)  # [b, c*p]
 
-        x = self.output_layer(prototype_similarities)  # [b, c]
+        x = torch.sum(prototype_similarities * self.output_weights[np.newaxis, ...], dim=-1)  # [b, c]
         return x, min_distances
 
     def training_step(self, batch: list[torch.Tensor], batch_idx: int) -> torch.Tensor:
@@ -175,10 +150,9 @@ class ProtoPNetMNIST(pl.LightningModule):
         y_one_hot = torch.nn.functional.one_hot(y, num_classes=self.n_classes).float()
         y_pred, min_distances = self.forward(x)
 
-        if (self.current_epoch == self.freeze_epoch) and (batch_idx == 0):
+        if (self.current_epoch == self.projection_epochs[-1]) and (batch_idx == 0):
             self.conv_root.requires_grad_(False)
             self.prototypes.requires_grad_(False)
-            self.output_layer.requires_grad_(True)
 
         if self.current_epoch in self.projection_epochs:
             if batch_idx == 0:  # make sure that no old prototype distances are present
@@ -190,6 +164,7 @@ class ProtoPNetMNIST(pl.LightningModule):
             self.push_projected_prototypes()
 
         self.log(f"weight", self.conv_root.layers[0].weight[0, 0, 0, 0], prog_bar=True, on_step=True, on_epoch=True)
+        self.log(f"prototype", self.prototypes[0, 0, 0, 0, 0], prog_bar=True, on_step=False, on_epoch=True)
 
         entropy_loss = torch.nn.CrossEntropyLoss()(y_pred, y_one_hot)
         cluster_loss = torch.mean(_get_min_in_cluster_distance(y, min_distances))
@@ -204,20 +179,16 @@ class ProtoPNetMNIST(pl.LightningModule):
                 # todo: l1 loss of last layer
         )
 
-        self.log(f"loss", loss, prog_bar=True, on_step=True, on_epoch=True)
-        # self.log(f"cluster_loss", cluster_loss, prog_bar=True, on_step=True, on_epoch=True)
-        # self.log(f"separation_loss", separation_loss, prog_bar=True, on_step=True, on_epoch=True)
+        self.log(f"loss", loss, prog_bar=True, on_step=True, on_epoch=False)
+        # self.log(f"cluster_loss", cluster_loss, prog_bar=False, on_step=True, on_epoch=False)
+        # self.log(f"separation_loss", separation_loss, prog_bar=False, on_step=True, on_epoch=False)
         # self.log(f"orthogonality_loss", slot_orthogonality_loss, prog_bar=True, on_step=True, on_epoch=True)
-        # self.log(f"minimum_proto_presence", torch.min(self.proto_presence), prog_bar=True, on_step=True, on_epoch=True)
 
         acc_calculator = Accuracy(task="multiclass", num_classes=self.n_classes).to(self.device)
         accuracy = acc_calculator(y_pred, y)
-        self.log(f"acc", accuracy, prog_bar=True, on_step=True, on_epoch=True)
+        self.log(f"acc", accuracy, prog_bar=True, on_step=False, on_epoch=True)
 
-        if self.current_epoch >= self.freeze_epoch:
-            return entropy_loss
-        else:
-            return loss
+        return loss
 
     @torch.no_grad()
     def update_projected_prototypes(

interpretable_mnist/train_proto_pool.py
@@ -6,6 +6,13 @@ from interpretable_mnist.data import load_mnist
 from interpretable_mnist.proto_pnet import ProtoPNetMNIST
 from interpretable_mnist.prototype_plot_utilities import plot_projected_prototype
 
+def _is_lazy_weight_tensor(p: torch.Tensor) -> bool:
+    from torch.nn.parameter import UninitializedParameter
+
+    if isinstance(p, UninitializedParameter):
+        return True
+    return False
+
 
 if __name__ == "__main__":
     torch.set_float32_matmul_precision("medium")
@@ -15,10 +22,17 @@ if __name__ == "__main__":
     trainer = pl.Trainer(
         accelerator="gpu",
         devices=1,
-        max_epochs=params.Training.n_epochs,
+        max_epochs=params.Training.projection_epochs[-1],
         default_root_dir=params.OUTS_BASE_DIR,
     )
     proto_model = ProtoPNetMNIST(params.Training())
+
+    for name, param in proto_model.named_parameters():
+        if _is_lazy_weight_tensor(param):
+            try:
+                print(f"{name} {param}")
+            except:
+                pass
 
 
     trainer.fit(
@@ -28,8 +42,8 @@ if __name__ == "__main__":
 
     for class_idx in range(10):
         class_prototypes = proto_model.projected_prototypes[class_idx]
-        for class_prototype in class_prototypes:
-            plot_projected_prototype(class_prototype)
+        for proto_idx, class_prototype in enumerate(class_prototypes):
+            plot_projected_prototype(class_prototype, title=f"class: {class_idx}, prototype: {proto_idx}")
 
 
     # plt.figure()

tests/test_proto_pnet.py
@@ -10,20 +10,20 @@ from interpretable_mnist import proto_pnet
 from interpretable_mnist.data import load_mnist
 
 
-def test_starting_connections_in_output_layer():
-    n_classes = 3
-    n_slots = 2
-    expected_weights = torch.Tensor([
-        [1., 0., 0.],
-        [1., 0., 0.],
-        [0., 1., 0.],
-        [0., 1., 0.],
-        [0., 0., 1.],
-        [0., 0., 1.]
-    ])
-    starting_weights = proto_pnet._get_starting_output_layer_class_connections(n_classes, n_slots)
+def test_prototype_distances_to_similarities():
+    n_samples_batch = 20
+    n_classes = 10
+    n_protos_per_class = 3
+    height, width = 3, 3
 
-    torch.testing.assert_allclose(starting_weights, expected_weights, rtol=0, atol=1e-11)
+    distances = torch.ones(size=(n_samples_batch, n_classes, n_protos_per_class, height, width)) * 0.5
+    distances[0, 0, 0, 0, 0] = 0.
+    distances[1, 0, 0, 0, 0] = 0.2
+    similarities, min_distances = proto_pnet.prototype_distances_to_similarities(distances)
+
+    assert similarities[0, 0, 0] > similarities[1, 0, 0]
+    assert torch.isclose(min_distances[0, 0, 0], torch.tensor(0.0), rtol=0, atol=1e-11)
+    assert torch.isclose(min_distances[1, 0, 0], torch.tensor(0.2), rtol=0, atol=1e-11)
 
 
 def test_prototypes_are_differentiated_when_calling_distance():
@@ -40,58 +40,29 @@ def test_prototypes_are_differentiated_when_calling_distance():
     assert model.prototypes.grad is not None
 
 
-def original_proto_pool_distance_implementation(x, prototype_vectors, prototype_shape):
-    # copied from [1]
-    ones = torch.nn.Parameter(torch.ones(prototype_shape), requires_grad=False)
-    '''
-    apply self.prototype_vectors as l2-convolution filters on input x
-    '''
-    x2 = x ** 2
-    x2_patch_sum = torch.nn.functional.conv2d(input=x2, weight=ones)
-
-    p2 = prototype_vectors ** 2
-    p2 = torch.sum(p2, dim=(1, 2, 3))
-    # p2 is a vector of shape (num_prototypes,)
-    # then we reshape it to (num_prototypes, 1, 1)
-    p2_reshape = p2.view(-1, 1, 1)
-
-    xp = torch.nn.functional.conv2d(input=x, weight=prototype_vectors)
-    intermediate_result = - 2 * xp + p2_reshape  # use broadcast
-    # x2_patch_sum and intermediate_result are of the same shape
-    distances = torch.nn.functional.relu(x2_patch_sum + intermediate_result)
-
-    return distances
-
-
 PROTOTYPES_SHAPE = (10, 1, 64, 1, 1)  # c, p, d, 1, 1
 
 
 @pytest.mark.parametrize(
-    "prototypes", [
-        (torch.ones(size=PROTOTYPES_SHAPE)),  # expected result: 0
-        (0.5 * torch.ones(size=PROTOTYPES_SHAPE)),  # expected result: (d * (1-0.5))**2 / d
-        (np.pi * torch.ones(size=PROTOTYPES_SHAPE)),  # expected result: (d * (1-pi))**2 / d
-        # some deviations to [1] for random inputs, but simpler implementation here seems
-        # to work better for third example
+    "prototypes, expc_result", [
+        (torch.ones(size=PROTOTYPES_SHAPE), (1 - 1)),  # expected: sqrt(1 / d * d * (1-1)) ** 2 = 1-1
+        ((0.5 * torch.ones(size=PROTOTYPES_SHAPE)), (1 - 0.5)),  # expected: sqrt(1 / d * d * (1-0.5)**2) = 1-0.5
+        (np.pi * torch.ones(size=PROTOTYPES_SHAPE), (math.pi - 1)),  # expected: sqrt(1 / d * d * |1-pi|)**2  = pi-1
     ]
 )
-def test_distance_implementation_equal_to_protopool(prototypes):
+def test_distance_implementation_equal_to_protopool(prototypes, expc_result):
     train_config = params.Training()
     train_config.n_classes = PROTOTYPES_SHAPE[0]
     train_config.n_protos_per_class = PROTOTYPES_SHAPE[1]
-    model = proto_pnet.ProtoPNetMNIST(train_config, prototype_depth=PROTOTYPES_SHAPE[1])
+    train_config.minkowski_distance_order = 2
+    model = proto_pnet.ProtoPNetMNIST(train_config, prototype_depth=PROTOTYPES_SHAPE[2])
     model.prototypes = torch.nn.Parameter(prototypes)
 
     n, k, h, w = 20, PROTOTYPES_SHAPE[2], 3, 3
     z = torch.ones(size=(n, k, h, w))
 
     distance = model._get_prototype_distances(z)
-    pool_proto_shape = (PROTOTYPES_SHAPE[0] * PROTOTYPES_SHAPE[1], *PROTOTYPES_SHAPE[2:])
-    prototypes = torch.flatten(prototypes, start_dim=0, end_dim=1)
-    distance_original = original_proto_pool_distance_implementation(z, prototypes, pool_proto_shape)
-
-    distance = torch.flatten(distance, start_dim=1, end_dim=2)
-    torch.testing.assert_allclose(distance, distance_original, rtol=0, atol=1e-3)
+    torch.testing.assert_allclose(distance, torch.full_like(distance, expc_result), rtol=0, atol=1e-3)
 
 
 def test_proto_pnet_model_runs():